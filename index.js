4960d7f781d1d2ca3bb1b0ab8b1d502c493c8722
viyan-najmaldin <najmadinviyan@gmail.com>
2022-09-13 22:15:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(array, surprise) {
+    let arr = [];
+    for (let i = 0; i < array.length; i++) {
+      arr.push(`Thank you, ${array[i]}, for the wonderful ${surprise} gift!`);
+    }
+    return arr;
+  }
+  console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "birthday"));
+  
+  function countDown(num) {
+    while (num >= 0) {
+      console.log(num--);
+    }
+  }
+  countDown(10);
